96120b282e4c16eea7dd9e08dab41af63b4b52f6
MahanteshPatil1214 <patilmahantesh642@gmail.com>
2026-08-14 13:16:13 +0530

    Lazy initialization Approach in  Singleton Design Pattern

--- src/LLD/SingletonDesignPattern/Calculator.java
+++ src/LLD/SingletonDesignPattern/Calculator.java
@@ -4,7 +4,22 @@ public class Calculator {
     int a;
     int b;
 
+    private static Calculator obj ;
+
+    private Calculator() {
+        System.out.println("Instance created");
+    }
+
     public int sum(){
         return a*b;
+    }
+
+//    Lazy initialization Approach
+    public static Calculator getInstance(){
+        if(obj == null) {
+            obj = new Calculator();
+        }
+        return obj;
+
     }
 }
--- src/LLD/SingletonDesignPattern/Main.java
+++ src/LLD/SingletonDesignPattern/Main.java
@@ -2,7 +2,16 @@ package LLD.SingletonDesignPattern;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Calculator calculator1 = new Calculator();
+        Calculator c1 = Calculator.getInstance();
+        Calculator c2 = Calculator.getInstance();
+        c1.a=3;
+        c1.b=10;
+
+
+        c1.a=4;
+        c1.b=1;
+        System.out.println(c1.sum());
+        System.out.println(c2.sum());
+
     }
 }
